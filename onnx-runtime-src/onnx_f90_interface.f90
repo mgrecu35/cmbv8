14e7f61d3194df0848c20797cf68954efea1309a
Mircea Grecu (UMD/GSFC) <mirgrecu@gmail.com>
2025-02-14 15:37:15 +0000

    updated onnx

---
+++ onnx-runtime-src/onnx_f90_interface.f90
@@ -0,0 +1,183 @@
+subroutine init_onnx_f90()
+    call init_onnx_runtime()
+    print*, 'here'
+end subroutine init_onnx_f90
+!call_onnx_(float *input_data, int *lengths_data, float *output_data, int *batch_size, int *seq_len, int *input_size, int *output_size)
+
+subroutine call_onnx_f90(input_data,actual_seq_len,n_input,n_seq,n_batch,&
+    n_output,output_data)
+    real :: input_data(n_input,n_seq,n_batch)
+    integer :: actual_seq_len(n_batch)
+    real,intent(out) :: output_data(n_output,n_seq,n_batch)
+    print*, n_input, n_seq, n_batch, n_output
+    call call_onnx(input_data, actual_seq_len, output_data, n_batch, & 
+         n_seq, n_input, n_output)
+end subroutine call_onnx_f90
+
+subroutine onnx_retrieval_ku_f90(z_ku_meas, p_type, bin_nodes, n_scans, onnx_precip_rate, onnx_dm, &
+    near_surf_onnx_precip_rate,n_batch, n_seq, n_input,n_output)
+    integer :: n_scans
+    real :: z_ku_meas(88,49,n_scans)
+    integer :: p_type(49,n_scans) , bin_nodes(5,49,n_scans)
+    real, intent(out) :: onnx_precip_rate(88,49,n_scans)
+    real, intent(out) :: onnx_dm(88, 49,n_scans)
+    integer :: n_batch, n_seq, n_input, n_output
+    real :: input_data(n_input,n_seq,n_batch)
+    integer :: actual_seq_len(n_batch)
+    real :: output_data(n_output,n_seq,n_batch)
+    integer :: i, j, k, ic
+    real :: z_ku_scaled, bin_scaled
+    real, intent(out) :: near_surf_onnx_precip_rate(49,n_scans)
+    integer :: n_points=1
+    ic=1
+    !print*, n_points,n_batch,n_seq,n_input,n_output
+    !print*, n_scans
+    onnx_dm=0
+    onnx_precip_rate=0
+    near_surf_onnx_precip_rate=0
+    
+    do i=1,n_scans
+        do j=1,49
+           if (p_type(j,i) > 0) then
+              !print*, p_type(j,i), bin_nodes(:,j,i)
+                do k=bin_nodes(1,j,i),bin_nodes(5,j,i)
+                    z_ku_scaled=z_ku_meas(k,j,i)
+                    if (z_ku_scaled<0) z_ku_scaled=0
+                    z_ku_scaled=(z_ku_scaled-12)/8
+                    bin_scaled=(k-bin_nodes(3,j,i))/8.0
+                    if (k+1-bin_nodes(1,j,i) <= n_seq) then
+                        input_data(1,k+1-bin_nodes(1,j,i),ic)=z_ku_scaled
+                        input_data(2,k+1-bin_nodes(1,j,i),ic)=bin_scaled
+                    end if
+                end do
+                actual_seq_len(ic)=bin_nodes(5,j,i)-bin_nodes(1,j,i)+1
+                !print*, i, j, ic, actual_seq_len(ic)
+                if (actual_seq_len(ic)>1) then
+                    call call_onnx(input_data, actual_seq_len, output_data, n_points, & 
+                    n_seq, n_input, n_output)
+                endif
+                do k=bin_nodes(1,j,i),bin_nodes(5,j,i)
+                    onnx_precip_rate(k,j,i)=0.1*(10**output_data(1,k+1-bin_nodes(1,j,i),ic)-1)
+                    onnx_dm(k,j,i)=output_data(2,k+1-bin_nodes(1,j,i),ic)
+                    if(k==bin_nodes(5,j,i)-1) then
+                        near_surf_onnx_precip_rate(j,i)=0.1*(10**output_data(1,k+1-bin_nodes(1,j,i),ic)-1)
+                    end if
+                end do
+            end if
+            
+        end do
+    end do
+    print*, n_input, n_seq, n_batch, n_output
+    print*, ic
+    
+end subroutine onnx_retrieval_ku_f90
+
+subroutine onnx_retrieval_f90(z_ku_meas, z_ka_meas, p_type, bin_nodes, n_scans, onnx_precip_rate, onnx_dm, &
+    near_surf_onnx_precip_rate,n_batch, n_seq, n_input,n_output)
+    integer :: n_scans
+    real :: z_ku_meas(88,49,n_scans)
+    real :: z_ka_meas(88,49,n_scans)
+    integer :: p_type(49,n_scans) , bin_nodes(5,49,n_scans)
+    real, intent(out) :: onnx_precip_rate(88,49,n_scans)
+    real, intent(out) :: onnx_dm(88, 49,n_scans)
+    integer :: n_batch, n_seq, n_input, n_output
+    real :: input_data(n_input,n_seq,n_batch)
+    integer :: actual_seq_len(n_batch)
+    real :: output_data(n_output,n_seq,n_batch)
+    integer :: i, j, k, ic
+    real :: z_ku_scaled, z_ka_scaled, bin_scaled
+    real, intent(out) :: near_surf_onnx_precip_rate(49,n_scans)
+    integer :: n_points=1
+    ic=1
+    print*, n_points,n_batch,n_seq,n_input,n_output
+    do i=1,n_scans
+        do j=1,49
+            if (p_type(j,i) > 0) then
+                do k=bin_nodes(1,j,i),bin_nodes(5,j,i)
+                    z_ku_scaled=z_ku_meas(k,j,i)
+                    if (z_ku_scaled<0) z_ku_scaled=0
+                    z_ku_scaled=(z_ku_scaled-12)/8
+                    z_ka_scaled=z_ka_meas(k,j,i)
+                    if (z_ka_scaled<0) z_ka_scaled=0
+                    z_ka_scaled=(z_ka_scaled-12)/8
+                    bin_scaled=(k-bin_nodes(3,j,i))/8.0
+                    if (k+1-bin_nodes(1,j,i) <= n_seq) then
+                        input_data(1,k+1-bin_nodes(1,j,i),ic)=z_ku_scaled
+                        input_data(2,k+1-bin_nodes(1,j,i),ic)=z_ka_scaled
+                        input_data(3,k+1-bin_nodes(1,j,i),ic)=bin_scaled
+                    end if
+                end do
+                actual_seq_len(ic)=bin_nodes(5,j,i)-bin_nodes(1,j,i)+1
+                !print*, i, j, ic, actual_seq_len(ic)
+                if (actual_seq_len(ic)>1) then
+                    call call_onnx(input_data, actual_seq_len, output_data, n_points, & 
+                    n_seq, n_input, n_output)
+                endif
+                do k=bin_nodes(1,j,i),bin_nodes(5,j,i)
+                    onnx_precip_rate(k,j,i)=0.1*(10**output_data(1,k+1-bin_nodes(1,j,i),ic)-1)
+                    onnx_dm(k,j,i)=output_data(2,k+1-bin_nodes(1,j,i),ic)
+                    if(k==bin_nodes(5,j,i)-1) then
+                        near_surf_onnx_precip_rate(j,i)=0.1*(10**output_data(1,k+1-bin_nodes(1,j,i),ic)-1)
+                    end if
+                end do
+            end if
+            
+        end do
+    end do
+    print*, n_input, n_seq, n_batch, n_output
+    print*, ic
+    
+end subroutine onnx_retrieval_f90
+
+subroutine onnx_retrieval_1d_f90(z_ku_meas, z_ka_meas,  bin_nodes, n_scans, onnx_precip_rate, onnx_dm, &
+    near_surf_onnx_precip_rate,n_batch, n_seq, n_input,n_output)
+    implicit none
+    integer :: n_scans
+    real :: z_ku_meas(88,n_scans)
+    real :: z_ka_meas(88,n_scans)
+    integer ::  bin_nodes(5,n_scans)
+    real, intent(out) :: onnx_precip_rate(88,n_scans)
+    real, intent(out) :: onnx_dm(88, n_scans)
+    integer :: n_batch, n_seq, n_input, n_output
+    real :: input_data(n_input,n_seq,n_batch)
+    integer :: actual_seq_len(n_batch)
+    real :: output_data(n_output,n_seq,n_batch)
+    integer :: i,  k, ic
+    real :: z_ku_scaled, z_ka_scaled, bin_scaled
+    real, intent(out) :: near_surf_onnx_precip_rate(n_scans)
+    integer :: n_points=1
+
+    ic=1
+    print*, n_points,n_batch,n_seq,n_input,n_output
+    do i=1,n_scans
+        do k=bin_nodes(1,i),bin_nodes(5,i)
+            z_ku_scaled=z_ku_meas(k,i)
+            if (z_ku_scaled<0) z_ku_scaled=0
+            z_ku_scaled=(z_ku_scaled-12)/8
+            z_ka_scaled=z_ka_meas(k,i)
+            if (z_ka_scaled<0) z_ka_scaled=0
+            z_ka_scaled=(z_ka_scaled-12)/8
+            bin_scaled=(k-bin_nodes(3,i))/8.0
+            if (k+1-bin_nodes(1,i) <= n_seq) then
+                input_data(1,k+1-bin_nodes(1,i),ic)=z_ku_scaled
+                input_data(2,k+1-bin_nodes(1,i),ic)=z_ka_scaled
+                input_data(3,k+1-bin_nodes(1,i),ic)=bin_scaled
+            end if
+        end do
+        actual_seq_len(ic)=bin_nodes(5,i)-bin_nodes(1,i)+1
+                !print*, i, j, ic, actual_seq_len(ic)
+        if (actual_seq_len(ic)>1) then
+            call call_onnx(input_data, actual_seq_len, output_data, n_points, & 
+                n_seq, n_input, n_output)
+        endif
+        do k=bin_nodes(1,i),bin_nodes(5,i)
+            onnx_precip_rate(k,i)=0.1*(10**output_data(1,k+1-bin_nodes(1,i),ic)-1)
+            onnx_dm(k,i)=output_data(2,k+1-bin_nodes(1,i),ic)
+            if(k==bin_nodes(5,i)) then
+                near_surf_onnx_precip_rate(i)=0.1*(10**output_data(1,k+1-bin_nodes(1,i),ic)-1)
+            end if
+        end do
+    end do
+    
+    
+end subroutine onnx_retrieval_1d_f90
--- src_c/onnx_bridge.c
+++ src_c/onnx_bridge.c
@@ -7,6 +7,11 @@
 
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 
+void onnx_retrieval_ku_f90_(float *z_ku_meas, int *p_type, int *bin_nodes, int *n_scans,
+			    float *onnx_precip_rate, float *onnx_dm, 
+			    float *near_surf_onnx_precip_rate, int *n_batch,
+			    int *n_seq,int * n_input, int *n_output);
+  
 void get_dprdata_c_loc_(float *zku,float *zka,int *rain_type,
 				    int *node, int *bin_sfc, float *freez_h);
 
@@ -16,6 +21,7 @@ void onnx_bridge(int ichunk, int orbnumber)
   int *rain_type,*node, *bin_sfc;
   float *freez_h;
   char *fname;
+  float *onnx_precip_rate, *onnx_dm, *near_surf_onnx_precip_rate;
   printf("ichunk=%i iorb=%i\n",ichunk,orbnumber);
   zku=(float *) malloc(sizeof(float)*300*49*88);
   zka=(float *) malloc(sizeof(float)*300*49*88);
@@ -23,10 +29,19 @@ void onnx_bridge(int ichunk, int orbnumber)
   node=(int *)malloc(sizeof(int)*300*49*5);
   bin_sfc=(int *)malloc(sizeof(int)*300*49);
   freez_h=(float *)malloc(sizeof(float)*300*49);
+  onnx_dm=(float *)malloc(sizeof(float)*300*49*88);
+  onnx_precip_rate=(float *)malloc(sizeof(float)*300*49*88);
+  near_surf_onnx_precip_rate=(float *)malloc(sizeof(float)*300*49);
   
   get_dprdata_c_loc_(zku,zka,rain_type,node,bin_sfc,freez_h);
   fname=(char *)malloc(sizeof(char)*100);
-  sprintf(fname, "npy_tmp_dir/cmb_npy_%2.2i_%6.6i.npz", ichunk, orbnumber);
+  int n_scans=300,n_batch=1,n_seq=60,n_input=2,n_output=2;
+  onnx_retrieval_ku_f90_(zku, rain_type, node, &n_scans,
+			 onnx_precip_rate, onnx_dm, 
+			 near_surf_onnx_precip_rate, &n_batch,
+			 &n_seq,&n_input,&n_output);
+  
+  sprintf(fname, "npy_tmp_dir/cmb_npy.%2.2i.%6.6i.npz", ichunk, orbnumber);
   printf("Filename: %s\n", fname);
 
   npy_array_list_t* list_var=NULL;
@@ -34,7 +49,9 @@ void onnx_bridge(int ichunk, int orbnumber)
 				  NPY_ARRAY_BUILDER_COPY( rain_type, SHAPE( 300, 49 ), NPY_DTYPE_INT32 ), "rain_type" );
   list_var= npy_array_list_append(list_var,
 				  NPY_ARRAY_BUILDER_DEEPCOPY( zku, SHAPE( 300, 49, 88 ), NPY_DTYPE_FLOAT32 ), "zku" );
-
+  list_var= npy_array_list_append(list_var, 
+				  NPY_ARRAY_BUILDER_COPY( near_surf_onnx_precip_rate,
+							  SHAPE( 300, 49 ), NPY_DTYPE_FLOAT32 ), "near_surface_onnx_precip_rate" );
   npy_array_list_save_compressed( fname, list_var, ZIP_CM_DEFAULT, 0 );
   npy_array_list_free(list_var);
   
@@ -44,4 +61,7 @@ void onnx_bridge(int ichunk, int orbnumber)
   free(node);
   free(bin_sfc);
   free(freez_h);
+  free(near_surf_onnx_precip_rate);
+  free(onnx_precip_rate);
+  free(onnx_dm);  
 }
